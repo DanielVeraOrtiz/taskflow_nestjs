ae33d0da328b1bcb6c0078ceaec2a00240512d42
Daniel <di.vera@uc.cl>
2026-06-10 15:44:00 -0400

    feat(appModule): Se configua configService para cargarr variables de entornos de los achivo .envs seg'un entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
+    // Puedo agrupar configuracion en otro archivo con registerAs y luego cargarlo con load aqui para
+    // mas orden.
+    ConfigModule.forRoot({
+      isGlobal: true, // Disponible en toda la app.
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      validationSchema: Joi.object({
+        JWT_SECRET: Joi.string().required(),
+      }),
+    }),
+    // Debo recordar que con servidores/proxies detras se rompe el rate limiting y debo habilitar trust
+    // proxy en main.ts.
     ThrottlerModule.forRoot([
       {
         ttl: 60000, // time to live de 60 seg.
