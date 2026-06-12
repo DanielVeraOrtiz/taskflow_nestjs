942412984fb32b990a49ff25b48ea9d77d18621c
Daniel <di.vera@uc.cl>
2026-06-12 18:08:58 -0400

    chore(dockerizado): Entorno dev dockerizado para avanzar

--- src/main.ts
+++ src/main.ts
@@ -38,7 +38,7 @@ async function bootstrap() {
   // Esta en la documentacion, agrega varios headers HTTP de seguridad en las respuestas, como
   // X-Content-Type-Options: nosniff, X-Frame-Options: SAMEORIGIN, Referrer-Policy: no-referrer.
   app.use(helmet());
-  await app.listen(configService.get<number>('PORT') ?? 3000);
+  await app.listen(configService.get<number>('PORT') ?? 3000, '0.0.0.0');
 }
 
 // Cambio debido a que me indica que las promesas deben ser esperadas con su catch y then.
