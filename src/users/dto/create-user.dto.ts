a9ea16a44e67b048bf262fead6732e9003d75a0f
Daniel <di.vera@uc.cl>
2026-06-16 11:29:47 -0400

    feat(user DTO): Se configuran los dtos de user con class validator y con swagger para documentacion

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,28 @@
-export class CreateUserDto {}
+import { IsString, IsEmail, MaxLength, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty()
+  @IsString()
+  @MaxLength(20)
+  @MinLength(4)
+  firstName!: string;
+
+  @ApiProperty()
+  @IsString()
+  @MaxLength(20)
+  @MinLength(4)
+  lastName!: string;
+
+  @ApiProperty()
+  @IsEmail()
+  @MaxLength(50)
+  @MinLength(4)
+  email!: string;
+
+  @ApiProperty()
+  @IsString()
+  @MinLength(8)
+  @MaxLength(100)
+  password!: string;
+}
